cfd5b861a3972bbb7298a5bbbde89387dda0153f
georgejeffery <georgejeffery@gmail.com>
2019-01-16 13:18:57 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+    @comment.user.build
+  end
+
   def create
-    comment = Comment.create(comment_params)
+    
+    if comment_params[:user_id].empty?
+      user = User.create(comment_params[:user_attributes])
+      comment = Comment.create(content: comment_params[:content], post_id: comment_params[:post_id], user:user)
+    else
+      comment = Comment.create(content: comment_params[:content], post_id: comment_params[:post_id])
+      user = User.find(comment_params[:user_id])
+      comment.user = user
+      comment.save
+      
+    end
     redirect_to comment.post
   end
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @user = User.new
   end
 
   def index
@@ -9,16 +11,44 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    2.times {Category.new}
   end
 
   def create
-    post = Post.create(post_params)
+    
+    categoryids = post_params[:category_ids].delete_if{|i| i==""}
+    if categoryids.empty? && post_params[:categories_attributes_0][:name].empty?
+      post = Post.create(title: post_params[:title], content: post_params[:content])      
+    elsif categoryids.empty? && !post_params[:categories_attributes_0][:name].empty?
+      post = Post.create(title: post_params[:title], content: post_params[:content])     
+      category = Category.create(name: post_params[:categories_attributes_0][:name])
+      post.categories << category
+      post.save
+    elsif !categoryids.empty? && !post_params[:categories_attributes_0][:name].empty?
+      post = Post.create(title: post_params[:title], content: post_params[:content])     
+      category1 = Category.create(name: post_params[:categories_attributes_0][:name])
+      post.categories << category1
+      post.save
+      categoryids.each do |id|
+        category = Category.find(id)
+        post.categories << category
+      end
+        post.save
+    else
+      post = Post.create(title: post_params[:title], content: post_params[:content])
+      categoryids.each do |id|
+        category = Category.find(id)
+        post.categories << category
+      end
+        post.save
+    end
+
     redirect_to post
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes_0: [:name])
   end
 end
